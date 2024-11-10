441057ce672f2b8a91861e375d4018d024d64936
G-ilian <gbiel.boza@gmail.com>
2024-11-10 15:55:12 -0500

    Initial Commit

---
+++ VidaPlenaQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VidaPlenaQuiz
+//
+//  Created by user270464 on 11/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VidaPlenaQuizUITests/VidaPlenaQuizUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  VidaPlenaQuizUITestsLaunchTests.swift
+//  VidaPlenaQuizUITests
+//
+//  Created by user270464 on 11/10/24.
+//
+
+import XCTest
+
+final class VidaPlenaQuizUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
